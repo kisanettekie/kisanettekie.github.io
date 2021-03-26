2e00a0bacd29c6008f6d2ba9c58299bfe1a64bb5
kisanettekie <ktekie@miu.edu>
2021-03-25 21:51:49 -0500

    version4

--- cs303/labW1D4/app.js
+++ cs303/labW1D4/app.js
@@ -1,64 +1,56 @@
 "use strict";
 
-
-
-
-//module.exports={ucFirst, checkSpam,truncate,extractCurrencyValue,getMaxSubSum,camelize}
+// exports.ucFirst = ucFirst;
+// exports.checkSpam = checkSpam;
+// exports.truncate = truncate;
+// exports.extractCurrencyValue= extractCurrencyValue;
+// exports.getMaxSubSum= getMaxSubSum;
+// exports.camelize= camelize;
 /**
-  * @param {String} str 
-  * @ return string return the first letter to upper case
-  */
-
-function ucFirst(str) {
-    let firstChar= str.charAt(0).toUpperCase();
-    let theRest= str.substr(1);
-    return firstChar + theRest;
+ * 
+ * @param {string} str 
+ * @returns {string} returns the first Upper case of the string
+ */
+function ucFirst(str){
+let fchar= str.charAt(0).toUpperCase()//S
+let strSpl = str.substr(1) //imon
+return fchar+strSpl // Simon
 
 }
-//console.log(ucFirst("kisanet"));
+console.log(ucFirst('simon'))
 
 /**
  * 
- * @param {String} str 
- * @returns {boolean} true is spam contains spam, else false;
+ * @param {string} str 
+ * @returns {boolean} returns true if contains spam, otherwise false
  */
-function checkSpam(str){
-    let low= str.toLowerCase();
-   if(low.includes("viagra") || low.includes("free xxxxx")) { 
-   return true;
-}else{
-    return false
-}
+function checkSpam(str) { 
+    let lowerStr = str.toLowerCase();
 
-}
-// console.log(checkSpam("buy ViAgRA now"));
-// console.log(checkSpam("free xxxxx"));
+    if(lowerStr.includes('viagra') || lowerStr.includes('free xxxxx')){
+        return true;
+    }
+    return false;
+  }
+  //console.log(checkSpam('buy ViAgRA now'))
 
-
- /**
-  * @param {String} str
-  * @param {Number} n
-  *@returns {String} trancuated the string equal to the number n.
-  *
-  */
-
-function truncate(str,n){
-        
-if(str.length<=20){ 
-return str 
-} 
-str = str.slice(0,n-1)
-return str.concat("…")
-      }
-//console.log(truncate("What I'd like to tell on this topic is:", 20))
-
-
- /**
-  * @param {String} str
-  * @returns {Number} 
- 
+/**
+ * 
+ * @param {string} str 
+ * @param {number} n 
+ * @returns {string} truncated string equal to the number
  */
-  function extractCurrencyValue(str){
+  function truncate(str,n){
+    // let st= str.slice(0,n)
+    if(str.length<=20){ 
+   return str 
+    } 
+    str = str.slice(0,n-1)
+    return str.concat("…")
+  }
+console.log(truncate("What I'd like to tell on this topic is:", 20))
+
+function extractCurrencyValue(str){
     str= str.slice(1)
     return +str;
 }
@@ -66,46 +58,62 @@ console.log(extractCurrencyValue('$120'))
 
 /**
  * 
- * @param {Array} arr of numbers
- * @returns {number} the total of the maximal subarray
- 
+ * @param {array} arr 
+ * @returns {number} returns the maximun subsum of the array
  */
- function getMaxSubSum(arr) {
-    let maxSum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        let sumFixedStart = 0;
-        for (let j = i; j < arr.length; j++) {
-            sumFixedStart += arr[j];
-            maxSum = Math.max(maxSum, sumFixedStart);
-        }
+function getMaxSubSum(arr){
+    let max = 0;
+    let sum = 0;
+    for(let num of arr){
+        sum = Math.max(num, sum +num);
+        max = Math.max(max, sum)
     }
-    return maxSum;
-  }
-   //console.log(getMaxSubSum([2, -8, 5, -1, 2, -3, 2]))
+    return max;
+}
+console.log((getMaxSubSum([-1, 2, 3, -9, 11]), 11))
 
 /**
  * 
  * @param {string} str 
- * @returns {string};
+ * @returns {string} returns camelize string
  */
- function camelize(str){
+function camelize(str){
+let sep; 
+if(typeof(sep)==="undefined"){
+    sep = "-";
+}
+let words = str.split(sep);
+let cap="";
+for(let i=1; i<words.length; i++){
+    let capWord = words[i]
+     cap = words[0]+capWord.charAt(0).toUpperCase()+capWord.slice(1)
+    
+}
+return cap;
+}
+//console.log(camelize("list-style-image"))
+
+/**
+ * 
+ * @param {string} str 
+ * @returns {string} returns camelized string
+ */
+function camelize(str){
  
     let camel = false;
     let camelString = '';
-   
+    
     for(let i = 0; i < str.length; i++){
-      if(str.charAt(i) === '-'){
-        camel = true;
-   
-      } else if(camel) {
-        camelString += str.charAt(i).toUpperCase();
-        camel = false;
-      } else {
-        camelString += str.charAt(i);
-      }
+    if(str.charAt(i) === '-'){
+    camel = true;
+    
+    } else if(camel) {
+    camelString += str.charAt(i).toUpperCase();
+    camel = false;
+    } else {
+    camelString += str.charAt(i);
+    }
     } 
     return camelString;
-  }
-   
-
-  //console.log(camelize("list-style-image"));
+    }
+    console.log(camelize("list-style-image"))
--- cs303/labW1D4/test.js
+++ cs303/labW1D4/test.js
@@ -4,11 +4,11 @@
 // const assert = require("assert");  //always need this with node
 // const myExports = require("./app.js");  //with node need the name of your file with your functions here
 // const getMaxSubSum = myExports.getMaxSubSum;  //do this for all of the functions used in the Mocha tests
-// const ucFirst = myExports.ucFirst; 
+// const ucFirst = myExports.ucFirst;
 // const checkSpam = myExports.checkSpam; 
 // const truncate = myExports.truncate; 
-// const camelize = myExports.camelize; 
 // const extractCurrencyValue = myExports.extractCurrencyValue;
+// const camelize = myExports.camelize; 
 
 
 /* eslint-disable quotes */
@@ -24,44 +24,43 @@ describe("ucFirst", function() {
 
 describe("checkSpam", function() {
   it('finds spam in "buy ViAgRA now"', function() {
-    assert.strictEqual(checkSpam("buy ViAgRA now"),true);
+    assert.strictEqual(checkSpam('buy ViAgRA now'),true);
   });
 
   it('finds spam in "free xxxxx"', function() {
-    assert.strictEqual(checkSpam("free xxxxx"),true);
+    assert.strictEqual(checkSpam('free xxxxx'),true);
   });
 
   it('no spam in "innocent rabbit"', function() {
-    assert.strictEqual(checkSpam("innocent rabbit"),false);
+    assert.strictEqual(checkSpam('innocent rabbit'),false);
   });
 });
 
 /* eslint-enable quotes */
 describe("truncate", function() {
   it("truncate the long string to the given length (including the ellipsis)", function() {
-    assert.equal(
+    assert.strictEqual(
       truncate("What I'd like to tell on this topic is:", 20),
       "What I'd like to te…"
     );
   });
 
   it("doesn't change short strings", function() {
-    assert.equal(
+    assert.strictEqual(
       truncate("Hi everyone!", 20),
       "Hi everyone!"
     );
   });
 
- });
+});
 
 describe("extractCurrencyValue", function() {
 
   it("for the string $120 returns the number 120", function() {
     assert.strictEqual(extractCurrencyValue('$120'), 120);
   });
-
-
 });
+
 describe("getMaxSubSum", function() {
     it("maximal subsum of [1, 2, 3] equals 6", function() {
       assert.equal(getMaxSubSum([1, 2, 3]), 6);
