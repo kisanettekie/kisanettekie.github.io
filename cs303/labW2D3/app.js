b02cfaf9d47d251c150ff08dc39ad7072b27d19a
kisanettekie <ktekie@miu.edu>
2021-03-31 20:53:26 -0500

    v1

---
+++ cs303/labW2D3/app.js
@@ -0,0 +1,46 @@
+"use strict"
+ //module.exports = { sum,factorial,fibonacchi };
+ 
+/**
+ * 
+ * @param {number} num 
+ * @returns{numner}number; 
+ */
+ 
+ function sum(num){
+     if(num===1){
+         return num;
+     }
+     else{
+         return num+sum(num-1)
+     }
+ }
+
+ console.log(sum(3))
+/**
+ * 
+ * @param {number} num 
+ * @returns {number};
+ */
+function factorial(num){
+    if(num===1){
+        return num;
+    }
+    else{
+        return num * factorial(num-1);
+    }
+}
+//console.log(factorial(5));
+/**
+ * 
+ * @param {number} num 
+ * @returns {number}number;
+ */
+function fibonacci(num){
+    if(num<=1){
+        return num;
+    }
+    else {
+        return fibonacci(num-1) + fibonacci(num-2);
+    }
+}
---
+++ cs303/labW2D3/test.js
@@ -0,0 +1,27 @@
+//"use strict"
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./myRecursion.js");
+// const sum = myExports.sum;
+// const factorial = myExports.factorial;
+// const fibonacchi = myExports.fibonacchi;
+ 
+  
+describe("numbers using recursion",function(){
+    it("get a sum number",function (){
+        assert.equal(sum(5),15)
+    })
+     
+    it("get a factor of a number",function (){
+        assert.equal(factorial(5),120)
+    })
+     
+    it("fibonacci of a number",function (){
+        assert.equal(fibonacci(8),21)
+    })
+     
+    })
+    
+    
+    
+    
+ 
